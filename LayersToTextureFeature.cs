069559bd1455a8a1fa410cdd8095913babbec1d6
CanopyMatt <63485551+CanopyMatt@users.noreply.github.com>
2024-07-25 11:39:47 -0400

    Update LayersToTextureFeature.cs

--- LayersToTextureFeature.cs
+++ LayersToTextureFeature.cs
@@ -5,23 +5,16 @@ using UnityEngine.Rendering.Universal;
 
 public class LayersToTextureFeature : ScriptableRendererFeature
 {
-    [System.Serializable]
-    public class Settings
-    {
-        public string textureName = "_GlobalLayerTexture";
-        public LayerMask layerMask = 0;
-        public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
-    }
-
-    public Settings settings = new();
-
-    private LayerToTexturePass layerToTexturePass;
+    public string textureName = "_LayersToTexture";
+    public LayerMask layerMask = 0;
+    public RenderPassEvent _event;
+    private RenderLayersToTexturePass layerToTexturePass;
 
     public override void Create()
     {
-        layerToTexturePass = new LayerToTexturePass(settings.textureName, settings.layerMask, name)
+        layerToTexturePass = new RenderLayersToTexturePass(textureName, layerMask, name)
         {
-            renderPassEvent = settings.renderPassEvent
+            renderPassEvent = _event
         };
     }
 
@@ -30,33 +23,33 @@ public class LayersToTextureFeature : ScriptableRendererFeature
         renderer.EnqueuePass(layerToTexturePass);
     }
 
-    private class LayerToTexturePass : ScriptableRenderPass
+    protected override void Dispose(bool disposing)
+    {
+        layerToTexturePass.Dispose();
+    }
+
+    private class RenderLayersToTexturePass : ScriptableRenderPass
     {
         private readonly string textureName;
         private readonly LayerMask layerMask;
         private RTHandle renderTarget;
-        private ProfilingSampler sampler;
+        protected ProfilingSampler sampler;
 
-        public LayerToTexturePass(string textureName, LayerMask layerMask, string name)
+        public RenderLayersToTexturePass(string textureName, LayerMask layerMask, string name)
         {
             this.textureName = textureName;
             this.layerMask = layerMask;
             sampler = new ProfilingSampler(name);
         }
 
-        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
         {
-            if (renderTarget != null)
-            {
-                RTHandles.Release(renderTarget);
-            }
-
-            renderTarget = RTHandles.Alloc(cameraTextureDescriptor.width, cameraTextureDescriptor.height, 1, DepthBits.None, cameraTextureDescriptor.graphicsFormat, FilterMode.Bilinear, TextureWrapMode.Clamp, name: textureName);
-
-            ConfigureTarget(renderTarget);
-            ConfigureClear(ClearFlag.All, Color.clear);
+            RenderTextureDescriptor colorDescriptor = renderingData.cameraData.cameraTargetDescriptor;
+            colorDescriptor.depthBufferBits = 0;
+            RenderingUtils.ReAllocateIfNeeded(ref renderTarget, colorDescriptor, name: textureName);
+            ConfigureTarget(renderTarget, renderingData.cameraData.renderer.cameraDepthTargetHandle);
+            ConfigureClear(ClearFlag.Color, Color.clear);
         }
-
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
@@ -64,40 +57,35 @@ public class LayersToTextureFeature : ScriptableRendererFeature
 
             using (new ProfilingScope(cmd, sampler))
             {
-                // Command buffer shouldn't contain anything, but apparently need to
-                // execute so DrawRenderers call is put under profiling scope title correctly
+                // Need to command buffer to execute so drawing renderers is put under profiling scope title correctly
                 context.ExecuteCommandBuffer(cmd);
                 cmd.Clear();
 
-                FilteringSettings filteringSettings = new(RenderQueueRange.all, layerMask);
-                DrawingSettings drawingSettings = CreateDrawingSettings(new ShaderTagId("UniversalForward"), ref renderingData, SortingCriteria.CommonOpaque);
+                // Create the render list desc to use on DrawRendererList
+                RendererListDesc rendererListDesc = new(new ShaderTagId("UniversalForward"), renderingData.cullResults, renderingData.cameraData.camera);
+                rendererListDesc.layerMask = layerMask;
 
-                var rendererListDesc = new RendererListDesc(new ShaderTagId("UniversalForward"), renderingData.cullResults, renderingData.cameraData.camera)
-                {
-                    sortingCriteria = drawingSettings.sortingSettings.criteria,
-                    renderQueueRange = filteringSettings.renderQueueRange,
-                    excludeObjectMotionVectors = false,
-                    layerMask = layerMask,
-                    overrideMaterial = null,
-                    overrideMaterialPassIndex = 0
-                };
-
+                // Create and draw the render list
                 RendererList rendererList = context.CreateRendererList(rendererListDesc);
-
                 cmd.DrawRendererList(rendererList);
+
+                // Pass our custom target to shaders as a Global Texture reference
                 cmd.SetGlobalTexture(textureName, renderTarget);
 
-                // Execute Command Buffer one last time and release it
-                // (Otherwise we get weird recursive list in Frame Debugger)
+                // Execute Command Buffer again and release it
+                // Otherwise we get recursive list in Frame Debugger
                 context.ExecuteCommandBuffer(cmd);
                 cmd.Clear();
                 CommandBufferPool.Release(cmd);
             }
         }
 
-        public override void OnCameraCleanup(CommandBuffer cmd)
+        public void Dispose()
         {
-            RTHandles.Release(renderTarget);
+            if (renderTarget != null)
+            {
+                RTHandles.Release(renderTarget);
+            }
         }
     }
 }
